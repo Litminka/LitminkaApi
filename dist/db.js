1297bbb4b7d70499ff38c8609658949183b1ef1c
Thenk0 <thenk0@mail.ru>
2023-02-02 11:09:54 +0400

    small fixes and updates

--- dist/db.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.prisma = void 0;
-const client_1 = require("@prisma/client");
-exports.prisma = global.prisma || new client_1.PrismaClient();
-if (process.env.NODE_ENV !== 'production')
-    global.prisma = exports.prisma;
-//# sourceMappingURL=db.js.map
--- dist/helper/sleep.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-function sleep(time) {
-    return new Promise((resolve) => setTimeout(resolve, time));
-}
-exports.default = sleep;
-//# sourceMappingURL=sleep.js.map
