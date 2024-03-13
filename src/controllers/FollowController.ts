9c9b97c6ecacdacf542336cf7b14866bca1d0e03
ArcKontyR <kontyrtime@mail.ru>
2024-03-13 21:53:53 +0500

    Updated follow controller

--- src/controllers/FollowController.ts
+++ src/controllers/FollowController.ts
@@ -2,13 +2,10 @@ import { Response } from "express";
 import { DeleteFollow, Follow, RequestWithAuth, RequestWithUser } from "../ts/index";
 import { RequestStatuses } from "../ts/enums";
 import FollowService from "../services/FollowService";
-import ForbiddenError from "../errors/clienterrors/ForbiddenError";
-import prisma from "../db";
 
 export default class FollowController {
 
     public static async follow(req: RequestWithUser, res: Response) {
-
         const user = req.auth.user;
         const animeId: number = req.params.animeId as unknown as number;
         const { groupName, type } = req.body as Follow;
@@ -19,7 +16,6 @@ export default class FollowController {
             message: "Anime followed successfully"
         })
     }
-
 
     // FIXME: Refactor to middleware
     public static async unfollow(req: RequestWithUser, res: Response) {
