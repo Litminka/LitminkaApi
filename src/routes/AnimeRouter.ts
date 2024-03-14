23af663324eef402b55c54719db3f06c25518faa
Thenk0 <vladikvd@gmail.com>
2024-03-14 23:26:18 +0600

    mini fix

---
+++ src/routes/AnimeRouter.ts
@@ -0,0 +1,9 @@
+import { Router } from 'express';
+import AnimeController from '../controllers/anime/AnimeController';
+import { wrap } from '../middleware/errorHandler';
+import AnimeRequest from '../requests/AnimeRequest';
+const router = Router();
+
+router.get("/:animeId", new AnimeRequest().send(), wrap(AnimeController.getSingleAnime)
+);
+export { router as animeRouter };
