dc52194488fada8c3f68608e1debcc64702de59a
Thease_Meanse <maks2002bytovskii@gmail.com>
2024-03-27 21:26:04 +0500

    refactor: remove CREATE_TEST from .env

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,7 @@
 interface config {
     ignoreLicensors: string[]
     ratingMinVotes: number
+    createTestData: boolean
 }
 
 export const config: config = {
@@ -9,5 +10,6 @@ export const config: config = {
      * ignore these licensors for geoban when parsing shikimori anime
      */
     ignoreLicensors: ["Wakanim", "Crunchyroll", "Netflix", "Dentsu", "Русский Репортаж", "2x2", "1C", "1С", "FAN", "Синема Галэкси", "Мега-Аниме", "Экспонента", "All Media Company"],
-    ratingMinVotes: 5
+    ratingMinVotes: 5,
+    createTestData: true
 }
