a402cbef6934dc13d2dcbabf32a420733688c493
Thease_Meanse <maks2002bytovskii@gmail.com>
2024-05-11 18:28:11 +0500

    docs: add JsDoc to groupArrSlice

--- src/helper/groupsplice.ts
+++ src/helper/groupsplice.ts
@@ -1,3 +1,9 @@
+/**
+ * Splits array to batches
+ * @param arr
+ * @param size
+ * @returns
+ */
 export default function groupArrSplice(arr: any[], size: number) {
     const output: any[] = [];
     for (let i = 0; i < arr.length; i += size) {
